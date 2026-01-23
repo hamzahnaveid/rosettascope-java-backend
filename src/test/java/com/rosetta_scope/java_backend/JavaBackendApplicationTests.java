c4c8a7f6f2d8330fb50b66c9e394afd0dc540d4e
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-01-23 03:16:23 +0000

    Initial commit

---
+++ src/test/java/com/rosetta_scope/java_backend/JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosetta_scope.java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
