a6264279f18a5c0623eb014a7ac23212eff2f6cf
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-01-30 02:31:21 +0000

    Additional endpoints for login and signup

--- src/main/java/com/rosetta_scope/java_backend/controller/UserController.java
+++ src/main/java/com/rosetta_scope/java_backend/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.rosetta_scope.java_backend.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,42 @@ public class UserController {
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@GetMapping("/login")
+	@ResponseBody
+	public HashMap<String, String> login(@RequestParam String email, @RequestParam String password) {
+		HashMap<String, String> response = new HashMap<String, String>();
+		
+		if (!userExists(email)) {
+			response.put("response", "fail");
+			return response;
+		}
+		
+		User user = userDao.findByEmail(email).get();
+		if (user.getPassword().equals(password)) {
+			response.put("response", "success");
+			return response;
+		}
+		else {
+			response.put("response", "fail");
+			return response;
+		}
+	}
+	
+	@PostMapping("/signup")
+	@ResponseBody
+	public HashMap<String, String> signup(@RequestBody User user) {
+		HashMap<String, String> response = new HashMap<String, String>();
+		
+		if (userExists(user.getEmail())) {
+			response.put("response", "fail");
+			return response;
+		}
+		
+		saveUser(user);
+		response.put("response", "success");
+		return response;
+	}
 	
 	@GetMapping("/user/{email}")
 	@ResponseBody
@@ -31,8 +70,11 @@ public class UserController {
 	}
 	
 	@PostMapping("/user-save")
-	public void saveUser(@RequestBody User user) {
+	@ResponseBody
+	public User saveUser(@RequestBody User user) {
 		userDao.save(user);
+		// Client-side throws error in Volley if the response does not return a value, even if it is successful
+		return user;
 	}
 	
 	@DeleteMapping("user-delete")
