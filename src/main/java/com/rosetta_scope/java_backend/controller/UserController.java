caa424aba51cb275735011c992c2233125444772
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-03-19 04:49:03 +0000

    Endpoint for getting Knowledge Test questions

--- src/main/java/com/rosetta_scope/java_backend/controller/UserController.java
+++ src/main/java/com/rosetta_scope/java_backend/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.rosetta_scope.java_backend.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rosetta_scope.java_backend.dao.ScoreDao;
 import com.rosetta_scope.java_backend.dao.UserDao;
 import com.rosetta_scope.java_backend.entity.Score;
 import com.rosetta_scope.java_backend.entity.User;
@@ -24,6 +30,9 @@ public class UserController {
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private ScoreDao scoreDao;
 	
 	@GetMapping("/login")
 	@ResponseBody
@@ -135,6 +144,54 @@ public class UserController {
 		
 		// Client-side throws error in Volley if the response does not return a value, even if it is successful
 		return user;
+	}
+	
+	@GetMapping("/knowledgeTest/{email}")
+	@ResponseBody
+	public List<Score> getQuestionsForKnowledgeTest(@PathVariable String email) {		
+		User user = getUserByEmail(email);
+		Map<String, Double> scoreMap = user.getConfidenceScores();
+		List<Score> knowledgeTestQuestions = new ArrayList<Score>();
+		
+		while (!scoreMap.isEmpty()) {
+			
+			if (knowledgeTestQuestions.size() >= 10) {
+				break;
+			}
+			
+			String proficientWord = Collections.max(scoreMap.entrySet(), Map.Entry.comparingByValue()).getKey();
+			List<Score> scoreList = scoreDao.findUserScoresByWord(email, proficientWord);
+			
+			knowledgeTestQuestions.add(scoreList.get(scoreList.size()-1));
+			// Get random entry in list of Scores excluding the last entry
+			if (scoreList.size() > 1) {
+				Random random = new Random();
+				int index = random.nextInt(0, scoreList.size()-1);
+				
+				// Ensure no duplicate questions
+				String word = scoreList.get(index).getWord();
+				
+				if (!word.equalsIgnoreCase(knowledgeTestQuestions.get(knowledgeTestQuestions.size()-1).getWord())) {
+					knowledgeTestQuestions.add(scoreList.get(index));
+				}
+				
+				else {
+					while (word.equalsIgnoreCase(knowledgeTestQuestions.get(knowledgeTestQuestions.size()-1).getWord()) && scoreList.size() != 1) {
+						index = random.nextInt(0, scoreList.size()-1);
+						word = scoreList.get(index).getWord();
+						if (!word.equalsIgnoreCase(knowledgeTestQuestions.get(knowledgeTestQuestions.size()-1).getWord())) {
+							knowledgeTestQuestions.add(scoreList.get(index));
+							break;
+						}
+						scoreList.remove(index);
+					}
+				}
+			}
+			
+			// For getting subsequent most proficient words on next iterations of loop
+			scoreMap.remove(proficientWord);
+		}
+		return knowledgeTestQuestions;
 	}
 
 }
